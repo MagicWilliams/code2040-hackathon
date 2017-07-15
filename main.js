00db1fc5d88d6e1045354be90ab645202302eedf
David Latimore <dgl@u.northwestern.edu>
2017-07-15 10:34:58 -0700

    Initializing of project app with React.js, jade as a preprocessor, and Node.js on the backend.

---
+++ main.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('views', __dirname + '/public/views');
+app.set('view engine', 'jade');
+app.set('port', (process.env.PORT || 2040));
+
+app.get('/', function (req, res) {
+  res.render('index',
+  { title : 'Home' }
+  )
+});
+
+app.listen(app.get("port"), function () {
+	console.log('Listening on port ' + app.get("port") + '.');
+});
