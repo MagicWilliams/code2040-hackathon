c8acf214c5b88eda0b7329a372ab102222070a6c
Abdul Ali <abdula@twitter.com>
2017-07-15 20:50:48 -0700

    added createuserprofile method

--- main.js
+++ main.js
@@ -13,7 +13,6 @@ admin.initializeApp({
 
 var db = admin.database();
 var ref = db.ref('/');
-
 var app = express();
 
 // Attach an asynchronous callback to read the data at our posts reference
@@ -37,3 +36,51 @@ app.get('/', function (req, res) {
 app.listen(app.get('port'), function () {
 	console.log('Listening on port ' + app.get('port') + '.');
 });
+
+// Template for the data to be stored
+// revieweesRef.set ({
+//    userId1: {
+//       name: "Person A",
+//       bio: "This is a test bio."
+//       // img: 'public/static/img/sample_profile_img'
+//       // facebook: https:www.facebook.com/profile.php?id=100002315758201
+//       // twitter: https:twitter.com/abdul_ali5
+//       // linkedin: https:www.linkedin.com/in/abdul-ali19/
+//       reviews: { reviewerId1: answers, reviewerId2: answers }
+//    },
+//    userId2: {
+//       name: "Person B",
+//       reviews: {reviewerId1: "review1", reviewerId2: "review2"}
+//    },
+// });
+
+function createUserProfile(userId, name, email /*imageUrl */) {
+  db.ref('users/' + userId).set({
+    username: name,
+    email: email,
+    //profile_picture : imageUrl
+  });
+}
+
+// note: review is an array of answers where each index is the answer to a
+// different question
+function submitReview(userId, reviewerId, review) {
+  var userRef = db.ref('users/' + userId)
+  var reviewsRef = userRef.child("reviews")
+  return userRef.once('value').then(function(snapshot) {
+    var reviews = snapshot.val().reviews
+  });
+  reviews[reviewerId] = review
+  reviewsRef.set(review);
+}
+
+userId1 = "userId1";
+reviewerId1 = "reviewerId1";
+review1 = ["answer1", "answer2"];
+
+reviewerId2 = "reviewerId2";
+review2 = ["answer3", "answer4"];
+
+createUserProfile(userId1, "Abdul", "aliabdul5@gmail.com")
+submitReview(userId1, reviewerId1, review1)
+submitReview(userId1, reviewerId2, review2)
