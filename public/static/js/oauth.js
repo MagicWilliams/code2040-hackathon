f1e1fe723960c1be95b9b22f5e24ec3a6849ae18
Emilio Aurea <emilioaurea@berkeley.edu>
2017-07-15 23:50:53 -0700

    Got rid of fb login. Using firebase login.

---
+++ public/static/js/oauth.js
@@ -0,0 +1,27 @@
+var provider = new firebase.auth.FacebookAuthProvider();
+// Optionally, add any scopes here
+// Optionally, set any custom params here
+
+function facebookSignin() {
+   firebase.auth().signInWithPopup(provider).then(function(result) {
+      var token = result.credential.accessToken;
+      var user = result.user;
+
+      console.log(token);
+      console.log(user);
+
+   }).catch(function(error) {
+      console.log(error.code);
+      console.log(error.message);
+   });
+}
+
+function facebookSignout() {
+   firebase.auth().signOut()
+   
+   .then(function() {
+      console.log('Signout successful!')
+   }, function(error) {
+      console.log('Signout failed')
+   });
+}
