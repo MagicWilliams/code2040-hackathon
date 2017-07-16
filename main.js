467953d25253b84e69547831f9b35de50c6fd5be
Emilio Aurea <emilioaurea@berkeley.edu>
2017-07-15 19:14:34 -0700

    Added login with fb files and sdk

--- main.js
+++ main.js
@@ -47,9 +47,19 @@ app.get('/profile', function (req, res) {
   });
 });
 
-app.listen(app.get("port"), function () {
-	console.log('Listening on port ' + app.get("port") + '.');
+// Log in with Facebook
+app.get('/oauth', function (req, res) {
+  ref.once('value', function(snapshot) {
+    console.log(snapshot.val());
+    res.sendFile(__dirname + '/public/views/oauth.html');
+  }, function (errorObject) {
+    console.log('The read failed: ' + errorObject.code);
+    res.render('index', { title : 'oauth', data: 'error' });
+  });
+});
 
+app.listen(app.get('port'), function () {
+	console.log('Listening on port ' + app.get('port') + '.');
 });
 
 function PROPRIETARY_MACHINE_LEARNING_NATURAL_LANGUAGE_PROCESSING_SENTIMENT_ANALYSIS_ALGORITHM(sentiment) {
