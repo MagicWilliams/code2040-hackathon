d71b1241e325e384fc5a7ba24b6f8539ea37924c
Jamal Pace <jamalkpace@gmail.com>
2017-07-15 18:52:25 -0700

    Configure browserify

---
+++ public/static/js/components/recs/Idk.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Idk = () =>
+  <h1>
+    Rendered correctly!!! Turn up!!
+  </h1>;
+
+export default Idk;
---
+++ public/static/js/components/recs/recs.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Idk from './Idk';
+
+ReactDOM.render(
+  <Idk />,
+  document.getElementById('content')
+);
